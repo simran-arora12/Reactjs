2c5c5dff95443408f606c3c3c086479af5796e3f
simran.arora@urbint.com <simranarora@simranaroraurbintcoms-MacBook-Pro.local>
2024-10-07 15:39:12 +0530

    Password Generator Project

---
+++ Project/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './index.css';
+import PasswordGenerator from './PasswordGenerator';
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <PasswordGenerator/>
+    </>
+  )
+}
+
+export default App
